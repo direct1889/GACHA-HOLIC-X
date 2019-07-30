dda09f512a7085a648405a802555932a3510ac59
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-07-30 14:39:49 +0900

    EditorExtension基礎、OnPostprocessorAudio()

---
+++ GACHA-HOLIC/Assets/Lib/Editor/AssetPostProcessorTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace du.Edit {
+
+    public class AssetPostProcessorTest : AssetPostprocessor {
+        private void OnPostprocessAudio(AudioClip audioClip) {
+            var audioImporter = assetImporter as AudioImporter;
+            string path = audioImporter.assetPath;
+            // Menu/以下は全部モノラル化
+            audioImporter.forceToMono = path.Contains("Menu");
+            // BGM/以下は全部バックグラウンド読み込み
+            audioImporter.loadInBackground = path.Contains("BGM");
+        }
+    }
+
+}
